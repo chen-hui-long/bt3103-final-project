211df721bd18beac3b5de832160c833878f97f6a
yt <a1669219514@gmail.com>
2021-04-06 14:24:52 +0800

    edit EditProfile

--- src/components/EditProfile.vue
+++ src/components/EditProfile.vue
@@ -1,74 +1,81 @@
-<template> 
-    <div class="edit">
-        
-        <div class="links">
-            <router-link id="home" to='/home'>Home / </router-link> 
-            <router-link id="profile" to='/profile'>Profile / </router-link>
-            <router-link id="edit" to='/editProfile'>Edit</router-link>
-        </div>
-
-        <form action="/action_page.php">
-            <label for="image">Select image: </label>
-            <input name="image" type="file" accept="image/*" v-on:change="changeImage">
-
-
-            <label for="fname">First name: </label>
-            <input name="fname" type="text" v-model="fname"/>
-
-            <label for="lname">Last name: </label>
-            <input name="lname" type="text" v-model="lname"/>
-
-            <input type="submit" on:click.prevent="update()" value="Update">
-
-        </form>
-
+<template>
+  <div class="edit">
+    <div class="links">
+      <router-link id="home" to="/home">Home / </router-link>
+      <router-link id="profile" to="/profile">Profile / </router-link>
+      <router-link id="edit" to="/editProfile">Edit</router-link>
     </div>
 
+    <form action="/action_page.php">
+      <div id="image-upload-div">
+        <div id="img">Upload your image:</div>
+        <input type="file" @change="changeImage" accept="image/*" />
+        <img v-if="this.image" :src="image" />
+        <img v-else :src="this.image" />
+      </div>
 
+      <label>Name: </label>
+      <input type="text" v-model="name" placeholder="Name" />
+      <button id="update" v-on:click="save">Update</button>
+    </form>
+  </div>
 </template>
 
-<script> 
-import db from '../firebase.js'
+<script>
+import db from "../firebase.js";
+import firebase from "@firebase/app";
+require("firebase/auth");
 
-export default{
-    data(){
-        return{
-            fname: "",
-            lname: "",
-            image: "",
-        }
+export default {
+  data() {
+    return {
+      userID: "",
+      name: "",
+      image: "",
+    };
+  },
+
+  methods: {
+    changeImage(event) {
+      const file = event.target.files.item(0);
+      const reader = new FileReader();
+      reader.addEventListener("load", this.imageLoaded);
+      reader.readAsDataURL(file);
+    },
+    imageLoaded(event) {
+      this.image = event.target.result;
     },
 
-    props:['userID'],
-
-    methods: {
-        // not sure about the image upload ans storage
-        /*
-        changeImage(e){
-            this.image = firebase.storage().ref().child(e) 
-        },
-        */
-        
-        fetchOrigin(){
-            db.collection("Users").doc(this.userID).get().then(snapshot => {
-                const data = snapshot.data()
-                this.fname = data.fname
-                this.lname = data.lname
-                this.image = data.image
-            })
-        },
-
-        update(){
-            db.collection("Users").doc(this.userID).update({
-                'fname':this.fname, 'lname':this.lname, 'image':this.image
-            }).then(() => alert("Update successfully!"))
-        }
+    fetchOrigin() {
+      db.collection("Users")
+        .doc(this.userID)
+        .get()
+        .then((doc) => {
+          const data = doc.data();
+          this.name = data.name;
+          this.image = data.image;
+        });
     },
 
-    created(){
-        this.fetchOrigin()
-    }
-}
+    save() {
+      db.collection("Users")
+        .doc(this.userID)
+        .update({
+          name: this.name,
+          image: this.image,
+        })
+        .then(() => {
+          alert("Update successfully!");
+        });
+      this.$router.push({ path: "/profile" });
+    },
+  },
+
+  created() {
+    this.userID = firebase.auth().currentUser.uid;
+    this.fetchOrigin();
+  },
+};
 </script>
 
 <style scoped>
@@ -108,5 +115,12 @@ form input {
 form label {
   float: left;
 }
+
+img {
+  width: 50%;
+}
 </style>
+
+
+
 
