0337701d5714dbb545fa14fbc1a708a82ec0307a
baojiankun33 <bjiankun@gmail.com>
2021-04-11 18:59:07 +0800

    updated faq page

--- src/components/Faq.vue
+++ src/components/Faq.vue
@@ -5,50 +5,47 @@
     <infoList v-for="(item, index) in dynamicList" :key="index" :user="item" >
       {{item.a}}
     </infoList> 
-   
+    
   </div>
 </template>
 
 <script>
+import InfoList from "./InfoList";
 export default {
   name: "Faq",
-  data() {
+  components: { InfoList },
+  data(){
     return {
-      dynamicList: [],
-    };
+      dynamicList: []
+    }
   },
   methods: {
-    prepareDynamicList() {
+    prepareDynamicList(){
       let list = [
-        {
-          question: "How do I make a purchase?",
-          a:
-            "Contact the bakeries directly through the platforms listed on their profiles! :-)",
-        },
-        {
-          question: "Can I list my restaurant here? ",
-          a: "Sorry, our website currently caters to home bakeries only! :-(",
-        },
-        {
-          question: "Didn't find the answer to your question?",
-          a: "Contact us directly @ EMAIL and we'll get back to you soon! :-)",
-        },
+        {question: "How do I make a purchase?", 
+        a: "Contact the bakeries directly through the platforms listed on their profiles! :-)"},
+        {question: "Can I list my restaurant here? ", 
+        a: "Sorry, our website currently caters to home bakeries only! :-("},
+        {question: "Didn't find the answer to your question?", 
+        a: "Contact us directly @ EMAIL and we'll get back to you soon! :-)"}
       ];
-      list.forEach((element) => {
-        this.dynamicList.push({ ...element, visible: false });
+      list.forEach(element => {
+       this.dynamicList.push({...element, visible: false});
       });
     },
     update(user) {
       this.user = user;
     }
   },
-  async created() {
+  async created(){
     await this.prepareDynamicList();
   },
+
 };
 </script>
 
 <style scoped>
+
 header {
   text-align: center;
   color: black;
@@ -57,51 +54,8 @@ header {
   margin-block: 40px;
 }
 
-* {
-  box-sizing: border-box;
-  transition: 0.5s;
-}
-body {
-  padding: 0 2rem;
-}
-/*** Specific styles ***/
-details {
-  margin-bottom: 1rem;
-  padding: 1rem;
-  border-radius: 0.25rem;
-  border: 1px solid darkslategray;
-  margin-left: 50px;
-  margin-right:50px;
-}
-summary {
-  list-style-type: none;
-  font-size: 1.25rem;
-  font-weight: bold;
-  cursor: pointer;
-  position: relative;
-  outline: none;
-  color: darkslategray;
-}
-summary::after {
-  content: "▼";
-  padding: 0.5rem;
-  position: absolute;
-  right: 0.25rem;
-  top: -0.5rem;
-  color: black;
-}
-details[open] {
-  background: whitesmoke;
-}
-details[open] summary {
-  color: dimgrey;
-}
-details[open] summary::after {
-  content: "▲";
-}
-/*** End specific styles ***/
-p {
-  color: dimgrey;
-  letter-spacing: 1.5;
+.faq {
+  text-align: center;
+  margin-bottom: 50px;
 }
 </style>
