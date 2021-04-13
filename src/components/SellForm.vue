eee794eee11e306388aa3e8d7fa6b40e57933ba6
baojiankun33 <bjiankun@gmail.com>
2021-04-14 00:59:35 +0800

    changed default photos

--- src/components/SellForm.vue
+++ src/components/SellForm.vue
@@ -262,11 +262,11 @@ export default {
       facebook: "",
       instagram: "",
       imageData1:
-        "https://scontent-xsp1-2.xx.fbcdn.net/v/t1.6435-9/170738773_10216095687820033_7199740432956840961_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=730e14&_nc_ohc=E6MAslcn5CAAX_HdRyk&_nc_ht=scontent-xsp1-2.xx&oh=41fee1f29827d4c25f56e61f58cd3ec2&oe=609C995A",
+        "https://scontent-xsp1-3.xx.fbcdn.net/v/t1.6435-9/171201553_10216095789422573_3709964538737173234_n.jpg?_nc_cat=111&ccb=1-3&_nc_sid=730e14&_nc_ohc=Nb-9ad9tJoMAX_ojuez&_nc_ht=scontent-xsp1-3.xx&oh=a4d897664821ff59bca7c1bd7480aaf3&oe=609C950D",
       imageData2:
-        "https://scontent-xsp1-2.xx.fbcdn.net/v/t1.6435-9/169937624_10216094457629279_3868980115832293243_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=730e14&_nc_ohc=-82cH4NY5NwAX-fgfxv&_nc_ht=scontent-xsp1-2.xx&oh=7bb08790a7c8d162dcef95c0fa0be55c&oe=609A5816",
+        "https://scontent-xsp1-2.xx.fbcdn.net/v/t1.6435-9/171467233_10216095740901360_501033987709844571_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=730e14&_nc_ohc=Z3QPUdD1hOkAX_oUerS&_nc_ht=scontent-xsp1-2.xx&oh=d0be32fce5bad1317ef088a28325942f&oe=609B0EC0",
       imageData3:
-        "https://scontent-xsp1-2.xx.fbcdn.net/v/t1.6435-9/169937624_10216094457629279_3868980115832293243_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=730e14&_nc_ohc=-82cH4NY5NwAX-fgfxv&_nc_ht=scontent-xsp1-2.xx&oh=7bb08790a7c8d162dcef95c0fa0be55c&oe=609A5816",
+        "https://scontent-xsp1-2.xx.fbcdn.net/v/t1.6435-9/171467233_10216095740901360_501033987709844571_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=730e14&_nc_ohc=Z3QPUdD1hOkAX_oUerS&_nc_ht=scontent-xsp1-2.xx&oh=d0be32fce5bad1317ef088a28325942f&oe=609B0EC0",
       imageData4:
         "https://scontent-xsp1-2.xx.fbcdn.net/v/t1.6435-9/171467233_10216095740901360_501033987709844571_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=730e14&_nc_ohc=Z3QPUdD1hOkAX_oUerS&_nc_ht=scontent-xsp1-2.xx&oh=d0be32fce5bad1317ef088a28325942f&oe=609B0EC0",
       order_details: "",
