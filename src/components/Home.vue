201773b8575e238c53fb0780a51a1030d1fc6e78
baojiankun33 <bjiankun@gmail.com>
2021-04-15 23:53:52 +0800

    fixed location filter

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -517,8 +517,8 @@ export default {
         if (this.checked.location.includes(location[i])) {
           return true;
         }
-        return false;
       }
+      return false;
     },
     check_deliver: function (delivery) {
       if (delivery == undefined) {
